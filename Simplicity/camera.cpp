be109a5c235e6fcc03b09b597076087deba30a80
Hubert <40564309+Qzername@users.noreply.github.com>
2024-12-25 03:36:42 +0100

    Add unfinished camera

--- Simplicity/camera.cpp
+++ Simplicity/camera.cpp
@@ -1,9 +1,5 @@
 #include "camera.h"
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 void Camera::Config(unsigned int shaderProgramID) {
 	this->shaderProgramID = shaderProgramID;
 }
@@ -15,7 +11,12 @@ void Camera::CalculateTransformations()
 	glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
 	glm::mat4 projection = glm::mat4(1.0f);
 	projection = glm::perspective(glm::radians(FOV), (float)800 / (float)600, 0.1f, 100.0f);
-	view = glm::translate(view, glm::vec3(pos.x, pos.y, pos.z));
+
+	glm::vec3 glmPos = glm::vec3(pos.x, pos.y, pos.z);
+
+	view = glm::translate(view, glmPos);
+
+	view = glm::lookAt(glmPos, glmPos + cameraFront, cameraUp);
 
 	unsigned int viewLoc = glGetUniformLocation(shaderProgramID, "view");
 	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
--- Simplicity/camera.h
+++ Simplicity/camera.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <glad/glad.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "transform.h"
 
 class Camera {
 public:
 	Transform transform;
+	glm::vec3 cameraFront, cameraUp;
 	float FOV = 45.0f;
 
 	void Config(unsigned int shaderProgramID);
