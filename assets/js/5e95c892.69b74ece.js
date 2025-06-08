10fa92e6e172edfe2328aeb05b8fd962202fe276
Qzername <40564309+Qzername@users.noreply.github.com>
2025-06-08 19:24:22 +0200

    Deploy website - based on f6fa6b3c34b1a27d524a7d6da62a7104622813d3

---
+++ assets/js/5e95c892.69b74ece.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksimplicity=self.webpackChunksimplicity||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>n});r(6540);var c=r(4164),i=r(5500),t=r(7559),u=r(2831),a=r(1656),l=r(4848);function n(e){return(0,l.jsx)(i.e3,{className:(0,c.A)(t.G.wrapper.docsPages),children:(0,l.jsx)(a.A,{children:(0,u.v)(e.route.routes)})})}}}]);
---
+++ assets/js/aba21aa0.6ce69b10.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksimplicity=self.webpackChunksimplicity||[]).push([[742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
